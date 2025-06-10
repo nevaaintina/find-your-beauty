843ac75ca5aa95d6714659cf0c0b2b90e0deb3fd
nevaaintina <nevaaintina26@gmail.com>
2025-06-10 22:13:26 +0700

    aku

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="wireframe-homepage">
+      <div class="div">
+        <div class="overlap">
+          <img class="urutan-penggunaan" src="img/urutan-penggunaan-skincare-foto-freepik-freepik.png" />
+          <div class="overlap-group"><div class="text-wrapper">Lihat Produk</div></div>
+          <p class="temukan-produk">Temukan Produk <br />Skincare Untuk <br />Kulit Sehat Berseri</p>
+        </div>
+        <p class="p">Your Skincare Adventure Starts Here</p>
+        <img class="gambar-kulit-kering" src="img/gambar-kulit-kering.png" />
+        <img class="gambar-kulit" src="img/gambar-kulit-sensitif.png" />
+        <img class="img" src="img/gambar-kulit-kombinasi.png" />
+        <img class="gambar-kulit-2" src="img/gambar-kulit-berminyak.png" />
+        <div class="text-wrapper-2">Kulit Kering</div>
+        <div class="overlap-2">
+          <div class="text"></div>
+          <div class="text-wrapper-3">Kulit Berminyak</div>
+        </div>
+        <div class="text-wrapper-4">Kulit Kombinasi</div>
+        <div class="text-wrapper-5">Kulit Sensitive</div>
+        <div class="text-wrapper-6">Kategori Unggulan</div>
+        <img class="element" src="img/468091806-18002750972692944-8655010469185877846-n.png" />
+        <img class="daftar-produk" src="img/daftar-produk-terlaris-SKINTIFIC.png" />
+        <img class="featured-image" src="img/featured-image-rekomendasi-produk-avoskin-1200x620-1.png" />
+        <img class="image" src="img/image-750x-64817d0f2f319.png" />
+        <div class="this-week-s-choice">This Week&#39;s Choice</div>
+        <div class="overlap-3">
+          <img class="product" src="img/product-1643346878-b85bac8d-3-800x800.png" />
+          <img class="element-hsa-f" src="img/1727160782-hsa-f.png" />
+          <img class="product-2" src="img/product-1628568872-rdn-aha-30-800x800.png" />
+          <img class="product-3" src="img/product-1648714079-innisfree-800x800.png" />
+        </div>
+        <div class="overlap-4">
+          <img class="skincare-terbaik" src="img/skincare-terbaik-untuk-menjaga-kesehatan-kulit.png" />
+          <p class="element-skincare-terbaik">9 Skincare Terbaik Untuk Menjaga <br />Kesehatan Kulit</p>
+          <p class="untuk-mendapatkan">
+            Untuk mendapatkan hasil yang maksimal, <br />Skincare perlu digunakan secara rutin. Ada produk <br />yang
+            bisa digunakan pada pagi dan siang hari, <br />ada pula yang khusus dipakai pada malam hari. <br />Anda bisa
+            memulai rutinitas perawatan kulit<br />dengan menggunakan basic skincare.
+          </p>
+          <div class="text-wrapper-7">Learn More</div>
+          <img class="line" src="img/line-37.svg" />
+        </div>
+        <div class="overlap-5">
+          <div class="rectangle"></div>
+          <div class="rectangle-2"></div>
+          <div class="text-wrapper-8">Find Your Glow App</div>
+        </div>
+        <div class="overlap-group-2">
+          <div class="rectangle-3"></div>
+          <img class="rectangle-4" src="img/rectangle-122.svg" />
+          <div class="rectangle-5"></div>
+          <p class="text-wrapper-9">R e v i e w s</p>
+          <p class="text-wrapper-10">P r o d u c t</p>
+          <p class="text-wrapper-11">R e c o m m e n d a t i on</p>
+          <p class="text-wrapper-12">F o r u m</p>
+          <p class="text-wrapper-13">A r t i c l e</p>
+          <p class="text-wrapper-14">W i s h l i s t</p>
+          <p class="text-wrapper-15">P r o f i l</p>
+          <img class="find-your-beauty" src="img/find-your-beauty-2.png" />
+          <div class="text-wrapper-16">Register / Login</div>
+          <div class="text-wrapper-17">Search</div>
+          <img class="header" src="img/header.png" />
+          <img class="group" src="img/group.png" />
+          <img class="vector" src="img/vector.svg" />
+        </div>
+        <div class="overlap-6">
+          <p class="text-wrapper-18">© 2025 Your Find Glow. All Rights Reserved</p>
+          <div class="rectangle-6"></div>
+          <div class="rectangle-7"></div>
+          <img class="line-2" src="img/line-38.svg" />
+          <img class="rectangle-8" src="img/rectangle-126.png" />
+          <p class="by-NAT-NEVA-AWALIA">By&nbsp;&nbsp; NAT [ NEVA AWALIA TIARA ].</p>
+          <p class="text-wrapper-19">© 2025 Your Find Glow. All Rights Reserved</p>
+        </div>
+        <div class="rectangle-9"></div>
+      </div>
+    </div>
+  </body>
+</html>
